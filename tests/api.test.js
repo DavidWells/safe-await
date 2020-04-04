faf7525f0721bf265abba93fe5042577ca9405c0
DavidWells <hello@davidwells.io>
2020-04-03 18:39:51 -0700

    add typeError test

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,7 +3,7 @@ import safeAwait from '../lib'
 
 test('Valid promise has data. [err, data]', async (t) => {
   const [err, data] = await safeAwait(promiseOne())
-  t.is(err, null)
+  t.is(err, undefined)
   t.is(data, 'data here')
 })
 
@@ -23,17 +23,37 @@ test('Error promise has instance of error. [err, data]', async (t) => {
  * Verify promises still throw native errors when deeper issue exists
  */
 
-test('throws on code syntaxError', async t => {
+test('throws on code syntax error "ReferenceError"', async t => {
   await t.throwsAsync(async () => {
     const [err, data] = await safeAwait(promiseWithSyntaxError())
-  }, {instanceOf: ReferenceError, message: 'madeUpThing is not defined'})
+  }, {
+    instanceOf: ReferenceError,
+    message: 'madeUpThing is not defined'
+  })
 })
 
-test('throws on code syntaxError in try/catch', async t => {
+test('throws on code syntax error "ReferenceError" in try/catch', async t => {
   try {
     const [err, data] = await safeAwait(promiseWithSyntaxError())
   } catch (e) {
     t.is(e instanceof ReferenceError, true)
+  }
+})
+
+test('throws on code "TypeError"', async t => {
+  await t.throwsAsync(async () => {
+    const [err, data] = await safeAwait(promiseWithTypeError())
+  }, {
+    instanceOf: TypeError,
+    message: 'Cannot read property \'lolCool\' of null'
+  })
+})
+
+test('throws on code "TypeError" in try/catch', async t => {
+  try {
+    const [err, data] = await safeAwait(promiseWithTypeError())
+  } catch (e) {
+    t.is(e instanceof TypeError, true)
   }
 })
 
@@ -53,6 +73,14 @@ function promiseThrows(doError) {
 function promiseWithSyntaxError(doError) {
   return new Promise((resolve, reject) => {
     console.log(madeUpThing)
+    return resolve('should not reach this')
+  })
+}
+
+function promiseWithTypeError(doError) {
+  return new Promise((resolve, reject) => {
+    const fakeObject = null
+    console.log(fakeObject.lolCool)
     return resolve('should not reach this')
   })
 }
