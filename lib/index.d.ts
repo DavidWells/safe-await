d0c65ffb435999b9ef716358712d9ec1664d65ec
DavidWells <hello@davidwells.io>
2025-12-03 22:06:05 -0800

    types

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-type PromiseOrFn<T> = Promise<T> | (() => Promise<T>);
-type ErrorFirst<T> = [Error | undefined, T | undefined];
-type DataFirst<T> = [T | undefined, Error | undefined];
-
-declare function safeAwait<T>(promise: PromiseOrFn<T | Error>, invert?: boolean): Promise<ErrorFirst<T>>;
-declare function safeInverse<T>(promise: PromiseOrFn<T | Error>): Promise<DataFirst<T>>;
-
-export { safeAwait as default, safeAwait, safeInverse };
